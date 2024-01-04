e0bbb84ac474d1ea54726b8a610389f856484e8f
Andriana3 <andrianavovchok@gmail.com>
2024-01-04 02:27:20 +0100

    summary

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FilterWrapper, FilterInput } from './Filter.styled';
+
+const Filter = ({ value, onChange }) => {
+  return (
+    <FilterWrapper>
+      Find contacts by name
+      <FilterInput
+        style={{
+          backgroundColor: '#ffffdd',
+          border: 'none',
+        }}
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+    </FilterWrapper>
+  );
+};
+
+export { Filter };
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
